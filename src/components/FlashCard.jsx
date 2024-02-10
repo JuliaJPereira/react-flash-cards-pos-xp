6845dfb2b1bb9d63ecbd222d1468eaaaaf5d162f
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-10 14:23:53 -0300

    Aula 4.2. react-flash-cards-v1 – Organização do projeto

---
+++ src/components/FlashCard.jsx
@@ -0,0 +1,10 @@
+export default function FlashCard() {
+  return (
+    <div
+      className="border p-2 w-64 h-32 flex flex-row items-center justify-center font-semibold text-xl"
+      style={{ fontFamily: "'JetBrains Mono', monospace" }}
+    >
+      FlashCard
+    </div>
+  );
+}
---
+++ src/pages/FlashCardsPage.jsx
@@ -0,0 +1,13 @@
+import FlashCard from '../components/FlashCard';
+import Header from './../components/Header';
+import Main from './../components/Main';
+export default function FlashCardsPage() {
+  return (
+    <>
+      <Header>react-flash-cards</Header>
+      <Main>
+        <FlashCard />
+      </Main>
+    </>
+  );
+}
