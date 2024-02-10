beeadde9975cf85a0a11c9d45ab847af5a4176ed
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-10 17:17:22 -0300

    Aula 4.5. react-flash-cards-v1 – Implementação de Radio Buttons – parte 1

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -6,19 +6,49 @@ import FlashCards from '../components/FlashCards';
 import Button from '../components/Button';
 import { useState } from 'react';
 import { helperShuffleArray } from '../helpers/arrayHelpers';
+import RadioButton from '../components/RadioButton';
 
 export default function FlashCardsPage() {
   const [allCards, setAllCards] = useState(allFlashCards);
+  const [showTitle, setShowTitle] = useState(true);
+
   function handleButtonClick() {
     const shuffledCards = helperShuffleArray(allCards);
     setAllCards(shuffledCards);
   }
+
+  function handleRadioShowTitleClick() {
+    setShowTitle(true);
+  }
+
+  function handleRadioShowDescriptionClick() {
+    setShowTitle(false);
+  }
+
   return (
     <>
       <Header>react-flash-cards</Header>
       <Main>
         <div className="text-center mb-4">
           <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-4 m-4">
+          <RadioButton
+            id="radioButtonShowTitle"
+            name="showInfo"
+            buttonChecked={showTitle}
+            onButtonClick={handleRadioShowTitleClick}
+          >
+            Mostrar título
+          </RadioButton>
+          <RadioButton
+            id="radioButtonShowDescription"
+            name="showInfo"
+            buttonChecked={!showTitle}
+            onButtonClick={handleRadioShowDescriptionClick}
+          >
+            Mostrar descrição
+          </RadioButton>
         </div>
         <FlashCards>
           {allCards.map(({ id, title, description }) => {
